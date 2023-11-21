1a325733ba0838a7e198837613357bb38e3fdb4a
Thiago <thiagovignoloferreira@gmail.com>
2023-11-21 09:31:10 +0100

    First.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module es.ieslosmontecillos.di_t2a432_fxmltableview {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens es.ieslosmontecillos.di_t2a432_fxmltableview to javafx.fxml;
+    exports es.ieslosmontecillos.di_t2a432_fxmltableview;
+}
